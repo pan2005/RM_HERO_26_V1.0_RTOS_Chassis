5a4e6876d155f3fad00c57e77e34a63396e0690f
pan2005 <3585413299@qq.com>
2025-12-16 11:12:55 +0800

    com_with_gimbal.c update

--- Tasks/com_with_gimbal.c
+++ Tasks/com_with_gimbal.c
@@ -18,6 +18,7 @@ static void On_Usart1_Data_Received(uint8_t *data, uint16_t len) {
     HAL_UART_Transmit(&huart1, data, len, 10);
     if (Protocol_Parse(data, len, &gimbal_info)) {
 
+
     }
 }
 
@@ -32,13 +33,14 @@ void communication_with_gimbal_task(void * argument) {
     GimbalInfo_t     my_info;
     com_with_gimbal_init();
     while (1) {
-        // position += local_rc_ctrl->rc.ch[1] / 660.0;
-        // my_info.pitch_position = position;
-        // my_info.shoot_gear = local_rc_ctrl->rc.s[0];
-        // Protocol_Pack_GimbalInfo(&gimbal_info, tx_buf, &tx_len);
-        // usart_tx_binary(&huart1, tx_buf, tx_len);
-        usart_printf(&huart1,"hello\r\n");
-        osDelay(50);
+         position += local_rc_ctrl->rc.ch[1] / 660.0;
+         my_info.pitch_position = position;
+         my_info.shoot_gear = local_rc_ctrl->rc.s[0];
+         Protocol_Pack_GimbalInfo(&my_info, tx_buf, &tx_len);
+         usart_tx_binary(&huart1, tx_buf, tx_len);
+        usart_printf(&huart1,"%d\r\n",gimbal_info.shoot_gear);
+        //usart_printf(&huart1,"hello\r\n");
+        osDelay(1000);
 
     }
 
